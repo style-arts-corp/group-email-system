3c687570bc910abfad030e21c6a60c483f9040fd
OdenCraftsman <oden13123@gmail.com>
2024-08-10 13:17:27 +0900

    add headerの追加

---
+++ hosting/src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+const Header: React.FC = () => {
+  return (
+    <header className="App-header">
+      <p>
+       メール自動送信 <br/><b>Matching HUB NAGAOKA</b>
+      </p>
+    </header>
+  )
+}
+
+export default Header
