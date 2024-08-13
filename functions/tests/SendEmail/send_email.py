d00afe0a8b63a0c3bf621e5385722d9bae1f8cc1
OdenCraftsman <oden13123@gmail.com>
2024-08-13 18:21:31 +0900

    feat #3 メール送信のロジック追加

---
+++ functions/tests/SendEmail/send_email.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+def send_email(sender_email, sender_password, receiver_email, subject, body):
+    # メッセージの作成
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+
+    # 本文の追加
+    message.attach(MIMEText(body, "plain"))
+
+    # SMTPサーバーへの接続とメール送信
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(sender_email, sender_password)
+        server.send_message(message)
+        print("メールが正常に送信されました。")
+
+# 使用例
+sender_email = os.getenv("SENDER_EMAIL")
+sender_password = os.getenv("SENDER_PASSWORD")
+receiver_email = os.getenv("RECEIVER_EMAIL")
+subject = "Gmail　自動送信　テスト"
+body = """
+昨日話してた打ち合わせ明日やりましょう！
+時間的に何時が良いでしょうか？
+"""
+
+send_email(sender_email, sender_password, receiver_email, subject, body)
