83f4b650bf857390d56220d83bd6a418f2fdcff5
OdenCraftsman <oden13123@gmail.com>
2024-09-05 02:40:02 +0900

    feat #4 メール送信用のAPI作成
    指定されたメールアドレスに対してメールを送信することができるAPIを作成
    
    bodyとtoのみ対応

--- functions/main.py
+++ functions/main.py
@@ -11,6 +11,7 @@ import flask
 from flask import request, jsonify
 from flask_cors import CORS
 
+from src.email.send_email import send_email
 from src.SpreadSheet.spreadsheet import SpreadSheet
 
 initialize_app()
@@ -46,6 +47,21 @@ def get_gspread_data_by_id():
     spread_sheet = SpreadSheet(id=id)
     data = spread_sheet.get_data()
     return jsonify(data), 200
+
+@app.route('/process_send_email', methods=['POST'])
+def process_send_email():
+    if request.method != "POST":
+        return "method not allowed", 405
+    data = request.get_json()
+    if not data or 'target_list' not in data:
+        return "Missing email in JSON body", 400
+    target_list = data['target_list']
+    content = data['content']
+    for target in target_list:
+        send_text = content.replace("{company}", target["company"]).replace("{name}", target["name"]).replace("{role}", target["role"])
+        subject = "「WorkSmart」について"
+        send_email(os.getenv("SENDER_EMAIL"), os.getenv("SENDER_PASSWORD"), target["email"], subject, send_text)
+    return "complete", 200
 
 @https_fn.on_request()
 def api(req: https_fn.Request) -> https_fn.Response:
