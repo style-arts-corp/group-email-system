57fc5115c84e9fe8bf871218d20f21f615858d13
OdenCraftsman <oden13123@gmail.com>
2024-09-23 02:28:25 +0900

    add #13 ログインフォームの作成

---
+++ hosting/src/components/loginForm/loginForm.tsx
@@ -0,0 +1,86 @@
+import React, { useState, ChangeEvent } from 'react';
+import {
+  Button,
+  TextField,
+  Box,
+  Typography,
+  Container,
+} from '@mui/material';
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+const LoginForm: React.FC = () => {
+  const [loginData, setLoginData] = useState<LoginData>({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Login attempt:', loginData);
+    // ここでログイン処理を実装します（例：APIリクエスト）
+  };
+
+  return (
+    <Container component="main" maxWidth="xs" sx={{margin: 'auto', mt: "10%" }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          ログイン
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, width: '100%' }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="メールアドレス"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={loginData.email}
+            onChange={handleInputChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="パスワード"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={loginData.password}
+            onChange={handleInputChange}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            ログイン
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default LoginForm;
