50900f0082274bd4ece9bac81ad157ed96692275
OdenCraftsman <oden13123@gmail.com>
2024-09-23 02:28:51 +0900

    add #13 ログインページの作成

---
+++ hosting/src/page/ LoginPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/Header";
+import LoginForm from "../components/loginForm/loginForm";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header />
+      <LoginForm/>
+    </>
+  );
+}
+
+export default LoginPage;
