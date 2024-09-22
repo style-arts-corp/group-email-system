cc6ef7f66cadc09618ae258b7e81f97df8a53346
OdenCraftsman <oden13123@gmail.com>
2024-09-23 04:14:39 +0900

    feat #13 browserRouterを活用したRoutingを実装

--- hosting/src/App.tsx
+++ hosting/src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 import MainPage from './page/ MainPage ';
+import LoginPage from './page/ LoginPage';
 
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<MainPage/>} />
+          <Route path={'/login'} element={<LoginPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
