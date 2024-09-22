0762f318c4a0a250f455017505c4311620e8d4aa
OdenCraftsman <oden13123@gmail.com>
2024-09-23 04:53:46 +0900

    feat #13 contextの作成によりLogin機能をLoginFormから除去

--- hosting/src/components/loginForm/loginForm.tsx
+++ hosting/src/components/loginForm/loginForm.tsx
@@ -1,10 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  onAuthStateChanged
-} from 'firebase/auth';
 import React, { useState, ChangeEvent, useEffect } from 'react';
+import { Navigate } from "react-router-dom";
 import {
   Button,
   TextField,
@@ -14,31 +9,21 @@ import {
   Alert,
 } from '@mui/material';
 import useLoginErrorDialog from './LoginErrorDialog';
+import { useAuth } from '../../context/AuthContext';
 
 interface LoginData {
   email: string;
   password: string;
 }
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-
 const LoginForm: React.FC = () => {
+  const {error, signIn} = useAuth();
+
   const [loginData, setLoginData] = useState<LoginData>({
     email: '',
     password: '',
   });
-  const [error, setError] = useState<string | null>(null);
+  // const [error, setError] = useState<string | null>(null);
 
   const {LoginErrorDialog, showLoginErrorDialog} = useLoginErrorDialog();
 
@@ -52,29 +37,8 @@ const LoginForm: React.FC = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setError(null);  // Reset any previous errors
-    try {
-      await signInWithEmailAndPassword(auth, loginData.email, loginData.password);
-      console.log('Login successful');
-      // ここでログイン成功後の処理を実装します（例：ホーム画面へのリダイレクト）
-    } catch (error) {
-      showLoginErrorDialog();
-      setError('メールアドレスとパスワードを確認してください。');
-      console.error('Login error:', error);
-    }
+    await signIn(loginData.email, loginData.password);
   };
-
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log('User is signed in');
-        console.log(user);
-        // ここでログイン成功後の処理を実装します（例：ホーム画面へのリダイレクト）
-      } else {
-        console.log('User is signed out');
-      }
-    });
-  });
 
   return (
     <Container component="main" maxWidth="xs" sx={{margin: 'auto', mt: "10%" }}>
